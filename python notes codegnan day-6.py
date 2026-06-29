0df63cfe5d00d3c8f223e8a1d65c363fae2b1f56
HarikaKirlampalli <kirlampalliharika@gmail.com>
2026-06-29 19:52:45 +0530

    Add files via upload
    🚀 Day 6/100 – Python Learning Journey
    📚 Topics Covered:
    ✅ Type conversions
    ✅ Built-in-functions
    I practiced converting one collection type into another and learned how different Python data structures store and organize data.
    #Python #PythonProgramming #DataAnalytics #LearningJourney #Codegnan #Coding #100DaysOfCode #OpenToWork

---
+++ python notes codegnan day-6.py
@@ -0,0 +1,79 @@
+'''
+DAY-6
+TYPE COVERSIONS
+----------------
+>>This is process of coverting one data type to another.
+**int to str,float
+1.num=25
+print(type(num))
+so=str(num)
+print(type(so))
+
+2.num=89
+num_2=float(num)
+print(num_2)
+print(type(num))
+so=str(num)
+print(type(so))
+
+**
+num=8.9
+how=str(num)
+print(num)
+print(type(num))
+num_2=int(num)
+print(num_2)
+
+***str to int,float,list,tuple
+1.
+hai="78"
+num=int(hai)
+hello="54.67"
+num_2=float(hello)
+print(num_2+num)
+
+2. to list:
+any_="python"
+con_=list(any_)
+print(con_)
+
+3. to tuple:
+any_="python"
+con_=tuple(any_)
+print(con_)
+
+***list to str,tuple,dict
+1.to str:
+var_=['p','y','t','h','o','n']
+text_= "".join(var_)
+print(text_)
+
+2.to tuple:
+var_=['p','y','t','h','o','n']
+text_= tuple(var_)
+print(text_)
+
+3.to dict:
+pyth_= [('a',1),('b',2)]
+conver= dict(pyth_)
+print(conver)
+
+**tuple to str, list
+1.to list:
+fam = (1,2,3,4)
+print(list(fam))
+
+2.to str:
+fam = ('h','e','l','l','o',)
+text2_="".join(fam)
+print(text2_)
+
+**Built-in-functions:
+str()
+float()
+int()
+list()
+dict()
+'''
+
+
